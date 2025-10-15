070c1935abac242cfd197324c91b202edb620da9
puriora <purioragame@gmail.com>
2025-10-14 22:33:08 -0700

    appear

---
+++ Assets/Scripts/Buttons/Appear.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Appear : MonoBehaviour
+{
+    public SpriteRenderer spriteToShow;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spriteToShow.enabled = true;
+        }
+    }
+}
