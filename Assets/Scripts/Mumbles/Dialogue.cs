c5d18ec1893d213d4c2b97cffe8d6cbd8104da29
puriora <purioragame@gmail.com>
2025-10-22 15:23:57 -0700

    wjewiewiu

--- Assets/Scripts/Mumbles/Dialogue.cs
+++ Assets/Scripts/Mumbles/Dialogue.cs
@@ -6,11 +6,60 @@ using TMPro;
 public class Dialogue : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
-    public string[] lines;
+
+    // public string[] starting = new string[] {
+    //     " ",
+    //     " ",
+    //     "This should play at the start",
+    //     "Test message 1",
+    //     "Test message 2",
+    //     "LONG LONG LONG LONG LONG LONG LONG LONG LONG TESSSST MESSAGE",
+    //     "I NEED bread.",
+    //     "I need bread baaaaaaaad."
+    // };
+    public string[] starting = new string[] {
+        "Hello, everyone!",
+        "Welcome to my bakery, I’m so happy to have all of you here!",
+        "Today, we’ll be making some delicious bread and we really knead to get a move on!",
+        "I’ve already gotten the flour and water, so the next thing we need is some yeast!",
+        "Let’s go, my sous-chef!"
+    };
+    public string[] a_yeast_y_friend = new string[] {
+        "Yay, we got the yeast! And a new friend! Wasn’t she beautiful?",
+        "Now, we have another friend to share our bread with!",
+        "We’re on a roll, haha!",
+        "Next up, we should get some salt. I can’t seem to remember where I put it though...",
+        "Hopefully, we can rise to the challenge and find it together!"
+    };
+
+    public string[] a_yeast_n_friend = new string[] {
+        "Yay, we got the yeast!",
+        "We’re on a roll, haha!",
+        "Next up, we should get some salt. I can’t seem to remember where I put it though...",
+        "Hopefully, we can rise to the challenge and find it together!"
+    };
+
+    public string[] a_salt_y_friend = new string[] {
+        "Wow… we really need to fix those doors, haha!",
+        "But at least we got the salt, thanks to you and Brine!",
+        "I’m sure they’ll love the bread too! Let’s not keep them waiting, okay?",
+        "No more loafing around! We’ve got a bread to bake!"
+    };
+
+    public string[] a_salt_n_friend = new string[] {
+        "Wow… we really need to fix those doors, haha!",
+        "But at least we got the salt!",
+        "I think that’s everything we need, which means no more loafing around!",
+        "We’ve got a bread to bake!"
+    };
+
+    private string[] lines;
+    private string scene;
     public float textSpeed;
 
     public GameObject checklist;
     public Story story;
+    public MainMenu MainMenu;
 
     public int index = 0;
 
@@ -19,6 +68,31 @@ public class Dialogue : MonoBehaviour
     {
         index = 0;
         textComponent.text = string.Empty;
+        if (!InventoryData.Instance.Has(IngredientData.Type.Yeast))
+        {
+            lines = starting;
+            scene = "Dark Room";
+        }
+        if (InventoryData.Instance.Has(IngredientData.Type.Yeast) & InventoryData.Instance.Has(CompanionData.Type.Fairy))
+        {
+            lines = a_yeast_y_friend;
+            scene = "Salt Room";
+        }
+        if (InventoryData.Instance.Has(IngredientData.Type.Yeast) & !InventoryData.Instance.Has(CompanionData.Type.Fairy))
+        {
+            lines = a_yeast_y_friend;
+            scene = "Salt Room";
+        }
+        if (InventoryData.Instance.Has(IngredientData.Type.Salt) & InventoryData.Instance.Has(CompanionData.Type.Ghost))
+        {
+            lines = a_salt_y_friend;
+            // final scene?
+        }
+        if (InventoryData.Instance.Has(IngredientData.Type.Salt) & !InventoryData.Instance.Has(CompanionData.Type.Ghost))
+        {
+            lines = a_salt_n_friend;
+            // final scene?
+        }
         StartDialogue();
     }
 
@@ -64,9 +138,9 @@ public class Dialogue : MonoBehaviour
         }
         else
         {
-            textComponent.text = string.Empty;
-            checklist.SetActive(true);
+            // checklist.SetActive(true);
             story.EndStory();
+            MainMenu.GoToScene(scene);
         }
     }
 }
