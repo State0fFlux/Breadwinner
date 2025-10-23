9fe962166f3ba6079270b53dcdd9988db08f5137
State0fFlux <bmanske505@gmail.com>
2025-10-22 17:39:09 -0700

    FINAL PUSH YAY

--- Assets/Scripts/MonoBehaviour/Collectible.cs
+++ Assets/Scripts/MonoBehaviour/Collectible.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public abstract class Collectible<T> : MonoBehaviour
 {
   public T data;
+  public AudioClip collectionSFX;
+  private AudioSource audioSrc;
+
+  void Start()
+  {
+    audioSrc = GetComponent<AudioSource>();
+  }
 
   void OnTriggerEnter2D(Collider2D other)
   {
     if (!other.CompareTag("Player")) return;
     AddToInventory(data);
+    audioSrc.PlayOneShot(collectionSFX);
     Destroy(gameObject);
   }
 
